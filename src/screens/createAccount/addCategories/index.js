ba4e0c88778fa7610c842eadf2978963798e38d5
Charlan Matter <mattergremio2@gmail.com>
2023-03-08 23:28:08 -0300

    implementing addCategory request on createAccount

--- src/screens/createAccount/addCategories/index.js
+++ src/screens/createAccount/addCategories/index.js
@@ -1,13 +1,118 @@
+import { useState } from 'react';
+import DefaultButton from '../../../components/button';
+import financesApi from '../../../utils/api';
 import * as S from './styles'
 
 export default function AddCategories({ route, navigation }) {
     const { userId } = route.params;
 
+    const [selectedExpenses, setSelectedExpense] = useState([]);
+    const [selectedEntry, setSelectedEntry] = useState([]);
+
+    const expenseCategory = [
+        'Despesas fixas',
+        'Despesas extras',
+        'Mercado',
+        'Lazer',
+    ];
+
+    const entryCategory = [
+        'Salario',
+        'Extras'
+    ]
+
+    const saveCategories = async (selectedExpenses, selectedEntry) => {
+        try {
+            const expenses = selectedExpenses.map(category => ({
+                name: category,
+                isSpent: true,
+            }))
+    
+            const entries = selectedEntry.map(category => ({
+                name: category,
+                isSpent: false,
+            }))
+    
+            const categories = [...expenses, ...entries]
+
+            const body = {
+                userId,
+                categories
+            }
+            
+            const result = await financesApi.post('/createAccount/categories', body, {
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+      
+            // navigation.navigate('AddCategories', { userId: userInfos.id });
+          } catch (error) {
+            const message = error.response.data.error;
+            setErrorMessage(message);
+          }
+    }
+
+    const handleCategorySelect = (category, type) => {
+        if (type === 'expense') {
+            const index = selectedExpenses.indexOf(category);
+
+            if (index > -1) { // Se a categoria já foi selecionada, remova ela do array
+                setSelectedExpense(selectedExpenses.filter(c => c !== category));
+            } else {  // Se a categoria não foi selecionada, adicione ela ao array
+                setSelectedExpense([...selectedExpenses, category]);
+            }
+        } else if (type === 'entry') {
+            const index = selectedEntry.indexOf(category);
+
+            if (index > -1) { // Se a categoria já foi selecionada, remova ela do array
+                setSelectedEntry(selectedEntry.filter(c => c !== category));
+            } else {  // Se a categoria não foi selecionada, adicione ela ao array
+                setSelectedEntry([...selectedEntry, category]);
+            }
+        }
+    }
+
     return (
         <S.Container>
             <S.Form>
                 <S.DescriptionLabel>Selecione as categorias que te interessam, posteriormente poderá adicionar outras.</S.DescriptionLabel>
-                <S.DescriptionLabel>{userId}</S.DescriptionLabel>
+                <S.Title>Categorias de gasto:</S.Title>
+                <S.CategoryArea>
+                    {
+                        expenseCategory.map((category, index) => (
+                            <S.CategoryItem
+                                key={index}
+                                selected={selectedExpenses.includes(category)}
+                                onPress={() => handleCategorySelect(category, 'expense')}
+                            >
+                                <S.CategoryLabel selected={selectedExpenses.includes(category)}>
+                                    {category}
+                                </S.CategoryLabel>
+                            </S.CategoryItem>
+                        ))
+                    }
+                </S.CategoryArea>
+                <S.Title>Categorias de entrada de dinheiro:</S.Title>
+                <S.CategoryArea>
+                    {
+                        entryCategory.map((category, index) => (
+                            <S.CategoryItem
+                                key={index}
+                                selected={selectedEntry.includes(category)}
+                                onPress={() => handleCategorySelect(category, 'entry')}
+                            >
+                                <S.CategoryLabel selected={selectedEntry.includes(category)}>
+                                    {category}
+                                </S.CategoryLabel>
+                            </S.CategoryItem>
+                        ))
+                    }
+                </S.CategoryArea>
+                <DefaultButton
+                    text={"Salvar"}
+                    handleMethod={() => saveCategories(selectedExpenses, selectedEntry)}
+                />
             </S.Form>
         </S.Container>
     );
--- src/screens/createAccount/addCategories/styles.js
+++ src/screens/createAccount/addCategories/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   background-color: #FFF;
   flex: 1;
-  justify-content: center;
   align-items: center;
 `
 
@@ -15,7 +14,30 @@ export const Form = styled.View`
 
 export const DescriptionLabel = styled.Text`
   color: #009999;
+  font-size: 18px;
+  margin: 4px 0px;
+`
+
+export const Title = styled.Text`
+  color: #009999;
   font-weight: bold;
   font-size: 18px;
   margin: 4px 0px;
+`
+export const CategoryArea = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 10px 0px;
+`
+
+export const CategoryItem = styled.TouchableOpacity`
+  background-color: ${props => props.selected ? '#009999' : '#e5f4f4'}; 
+  padding: 8px 10px;
+  border-radius: 10px;
+  margin: 0px 7px 7px 0px;
+`
+
+export const CategoryLabel = styled.Text`
+  font-size: 20px;
+  color: ${props => props.selected ? '#e5f4f4' : '#009999'};
 `
